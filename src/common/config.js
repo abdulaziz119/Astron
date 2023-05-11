8087b7c8607405cdc1d8efe373c5e13f98f38074
abdualzizZz <119160854+abdualzizZz@users.noreply.github.com>
2023-05-11 16:42:51 +0500

    first commit

--- src/common/config.js
+++ src/common/config.js
@@ -8,7 +8,5 @@ DotEnv.config({
 const ENV = {
   DB_URL: process.env.DB_URL || "mongodb://localhost:27017/astron",
   PORT: process.env.PORT || 5500,
-  BOT_TOKEN:
-    process.env.BOT_TOKEN || "6039096389:AAE1JL2ZOQVjxUl_3j-vricszb4oXmqvcGg",
 };
 export default ENV;
