5c114fd32b84291759fbd0e86be545389d1294a1
abdulaziz <a.xazratqulov@1it.uz>
2024-04-06 17:42:51 +0500

    create

--- src/Astron/router/users/users.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-const router = Router();
-
-import { usersCreateHandler } from "../../handler/users/users.handler.js";
-
-import { usersAnchovyValidation } from "../../middleware/users/users.validation.js";
-
-router.post("/", usersAnchovyValidation, usersCreateHandler);
-export default router;
--- src/common/utils/jwt.js
+++
@@ -1,6 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export default {
-  sign: (data) => jwt.sign(data, "KEY1"),
-  verify: (token) => jwt.verify(token, "KEY1"),
-};
